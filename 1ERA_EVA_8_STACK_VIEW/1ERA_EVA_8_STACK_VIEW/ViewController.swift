4ea9cb3d37b7540784b41c8d496e2e3bf6446f21
José Martínez <ppmtzo@outlook.com>
2017-03-03 18:31:53 -0700

    Add files via upload

---
+++ 1ERA_EVA_8_STACK_VIEW/1ERA_EVA_8_STACK_VIEW/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1ERA_EVA_8_STACK_VIEW
+//
+//  Created by JOSE CARLOS on 13/02/17.
+//  Copyright © 2017 JOSE CARLOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 1ERA_EVA_8_STACK_VIEW/1ERA_EVA_8_STACK_VIEWTests/_ERA_EVA_8_STACK_VIEWTests.swift
@@ -0,0 +1,36 @@
+//
+//  _ERA_EVA_8_STACK_VIEWTests.swift
+//  1ERA_EVA_8_STACK_VIEWTests
+//
+//  Created by JOSE CARLOS on 13/02/17.
+//  Copyright © 2017 JOSE CARLOS. All rights reserved.
+//
+
+import XCTest
+@testable import _ERA_EVA_8_STACK_VIEW
+
+class _ERA_EVA_8_STACK_VIEWTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
